86309526c41d1d27fc4de085ea96355a5b6099ff
choldgraf <choldgraf@gmail.com>
2025-11-15 10:07:00 -0800

    GH

--- docs/plugins/github-issue-links/plugin.mjs
+++ docs/plugins/github-issue-links/plugin.mjs
@@ -18,7 +18,7 @@ async function fetchIssue(repoSlug, issueNumber) {
     return issueCache.get(cacheKey);
   }
   const headers = { Accept: 'application/vnd.github+json' };
-  const token = process?.env?.GITHUB_TOKEN;
+  const token = process?.env?.GH_TOKEN;
   if (token) {
     headers.Authorization = `Bearer ${token}`;
   }
